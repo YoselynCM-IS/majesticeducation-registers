32d97186bc7e657062c3dc8a3610066fb758dff6
Genaro Perez <genaromajestice@gmail.com>
2023-07-06 07:30:43 -0600

    codigos en pack etla

--- app/Exports/EmailsExport.php
+++ app/Exports/EmailsExport.php
@@ -24,15 +24,23 @@ class EmailsExport implements FromCollection
     {
         if($this->book === 'null'){
             // BUSQUEDA POR ESCUELA
-            $students = Student::select('schools.name as school', 'book', 'books.editorial', 'students.name', 'email')
+            $stdspart1 = Student::select('schools.name as school', 'book', 'books.editorial', 'students.name', 'email')
                 ->join('schools', 'schools.id', '=', 'students.school_id')
                 ->join('books', 'students.book', '=', 'books.name')
-                ->where('book', 'like', '%DIGITAL%')
-                ->where('book', 'NOT LIKE', '%PACK%')
                 ->where('check', 'accepted')
                 ->where('codes', false) 
-                ->where('schools.name', $this->school)
-                ->orderBy('students.created_at', 'asc')->get();
+                ->where('schools.name', $this->school);
+
+            if($this->school == 'INSTITUTO TECNOLÓGICO DE VALLE DE ETLA'){
+                $students = $stdspart1->where(function($query) {
+                                $query->where('book', 'like', '%DIGITAL%')
+                                        ->orWhere('book', 'like', '%PACK%');
+                                })->orderBy('students.created_at', 'asc')->get();
+            } else {
+                $students = $stdspart1->where('book', 'like', '%DIGITAL%')
+                                    ->where('book', 'NOT LIKE', '%PACK%')
+                                    ->orderBy('students.created_at', 'asc')->get();
+            }
         }
         if($this->school === 'null'){
             // BUSQUEDA POR LIBRO
