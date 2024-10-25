ab7aee28fc9e632145fbbb7eefe0975c4f2be36f
Yose30 <yos_23@live.com.mx>
2024-10-24 18:28:04 -0600

    Update SendRegisters.php

--- app/Mail/SendRegisters.php
+++ app/Mail/SendRegisters.php
@@ -38,8 +38,8 @@ class SendRegisters extends Mailable
         $nombre = $fecha.'-registros'.'.xlsx';
 
         if(env('APP_NAME') == 'MAJESTIC EDUCATION'){
-            $from = 'registro.pagos@majesticeducation-registers.com';
-            $cc = "rp.majesticeducacion@gmail.com";
+            $cc = 'registro.pagos@majesticeducation-registers.com';
+            // $cc = "rp.majesticeducacion@gmail.com";
         } else {
             $from = 'registro.pagos@omegabook-registers.com';
             $cc = "rp.omegabook@gmail.com";
